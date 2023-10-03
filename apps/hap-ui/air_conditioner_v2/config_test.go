7ad582d9bd80282cd8369f4974c2986a2343e5d8
Nikita Sivakov <sivakov512@icloud.com>
2023-10-04 01:32:41 +0200

    Copy config implementation to new air conditioner facade

---
+++ apps/hap-ui/air_conditioner_v2/config_test.go
@@ -0,0 +1,41 @@
+package airconditionerv2_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"hap-ui/air_conditioner"
+	"testing"
+)
+
+func TestLoadsConfigCorrectly(t *testing.T) {
+	got, err := airconditioner.LoadConfig("../testing_fixtures/valid_config.toml")
+
+	assert.Nil(t, err)
+	assert.Equal(t, &airconditioner.Config{
+		Manufacturer: "Midea",
+		Name:         "AC",
+		PIN:          "11122333",
+		Cooling: airconditioner.Temperature{
+			Min:  9.5,
+			Max:  31.5,
+			Step: 0.8,
+		},
+		Heating: airconditioner.Temperature{
+			Min:  0.5,
+			Max:  31.5,
+			Step: 0.8,
+		},
+		MQTT: airconditioner.MQTT{
+			Broker:      "tcp://iot.eclipse.org:1883",
+			UpdateTopic: "ac/update/LivingRoom",
+			StatusTopic: "ac/status/LivingRoom",
+		},
+	}, got)
+
+}
+
+func TestErroredForWrongConfig(t *testing.T) {
+	got, err := airconditioner.LoadConfig("../testing_fixtures/invalid_config.toml")
+
+	assert.NotNil(t, err)
+	assert.Nil(t, got)
+}
