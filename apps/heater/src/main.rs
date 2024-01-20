a707b3916809e4d74b9bc01254a00f7d0a590899
Nikita Sivakov <sivakov512@icloud.com>
2024-01-20 23:43:25 +0100

    Add logging a bit

--- apps/heater/src/main.rs
+++ apps/heater/src/main.rs
@@ -15,6 +15,7 @@ async fn main() {
 
     let config_fpath = std::env::var(CONFIG_ENV_VAR).unwrap_or(CONFIG_DEFAULT.to_owned());
     let config = Config::from_config_file(&config_fpath).unwrap();
+    log::info!("Config successfully loaded from {}", config_fpath);
 
     Device::new(config).run().await.unwrap();
 }
