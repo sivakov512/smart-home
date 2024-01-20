f1407d4a16af813315c23da41902abb8c378d00a
Nikita Sivakov <sivakov512@icloud.com>
2024-01-20 23:16:47 +0100

    Implement device restore from previous state

--- apps/heater/src/state.rs
+++ apps/heater/src/state.rs
@@ -22,6 +22,12 @@ impl From<&State> for Vec<u8> {
     }
 }
 
+impl From<&[u8]> for State {
+    fn from(input: &[u8]) -> Self {
+        serde_json::from_slice(input).unwrap()
+    }
+}
+
 impl State {
     pub fn update(&mut self, updates: StateUpdate) {
         self.is_active = updates.is_active;
@@ -46,6 +52,26 @@ impl From<&[u8]> for StateUpdate {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::*;
+
+    #[fixture]
+    fn state() -> State {
+        State {
+            is_active: true,
+            mode: Mode::Heat,
+            current_temperature: 21.3,
+            target_temperature: 23.5,
+        }
+    }
+
+    #[fixture]
+    fn state_update() -> StateUpdate {
+        StateUpdate {
+            is_active: false,
+            mode: Mode::Idle,
+            target_temperature: 28.7,
+        }
+    }
 
     #[test]
     fn default_state_looks_as_expected() {
@@ -62,16 +88,9 @@ mod tests {
         )
     }
 
-    #[test]
-    fn state_encoded_as_expectedf() {
-        let input = State {
-            is_active: true,
-            mode: Mode::Heat,
-            current_temperature: 21.3,
-            target_temperature: 23.5,
-        };
-
-        let got = Vec::<u8>::from(&input);
+    #[rstest]
+    fn state_encoded_as_expectedf(state: State) {
+        let got = Vec::<u8>::from(&state);
 
         assert_eq!(
             String::from_utf8(got).unwrap(),
@@ -79,45 +98,36 @@ mod tests {
         );
     }
 
-    #[test]
-    fn state_updates_as_expected() {
-        let mut state = State {
-            is_active: false,
-            mode: Mode::Idle,
-            current_temperature: 21.3,
-            target_temperature: 23.5,
-        };
+    #[rstest]
+    fn state_decoded_as_expected(state: State) {
+        let input = r#"{"is_active":true,"mode":"heat","current_temperature":21.3,"target_temperature":23.5}"#;
 
-        state.update(StateUpdate {
-            is_active: true,
-            mode: Mode::Heat,
-            target_temperature: 25.0,
-        });
+        let got = State::from(input.as_bytes());
+
+        assert_eq!(got, state)
+    }
+
+    #[rstest]
+    fn state_updates_as_expected(mut state: State, state_update: StateUpdate) {
+        state.update(state_update);
 
         assert_eq!(
             state,
             State {
-                is_active: true,
-                mode: Mode::Heat,
+                is_active: false,
+                mode: Mode::Idle,
                 current_temperature: 21.3,
-                target_temperature: 25.0
+                target_temperature: 28.7
             }
         );
     }
 
-    #[test]
-    fn state_update_decoded_as_expected() {
-        let input = r#"{"is_active": true, "mode": "heat", "target_temperature": 23.5}"#;
+    #[rstest]
+    fn state_update_decoded_as_expected(state_update: StateUpdate) {
+        let input = r#"{"is_active":false,"mode":"idle","target_temperature":28.7}"#;
 
         let got = StateUpdate::from(input.as_bytes());
 
-        assert_eq!(
-            got,
-            StateUpdate {
-                is_active: true,
-                mode: Mode::Heat,
-                target_temperature: 23.5
-            }
-        )
+        assert_eq!(got, state_update)
     }
 }
